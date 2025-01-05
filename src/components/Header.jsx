9454bef6a7dd94a60c6dfa34569cfd73ea19f519
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-01-05 12:23:50 +0530

    Made some changes for login in the header part and also add font styling, bg-color in the index.css file.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../assets/logo.png";
 import LogoSymbol from "../assets/LogoSymbol.png";
 import Search from "./Search";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { BsCart4 } from "react-icons/bs";
 import useMobile from "../hooks/useMobile";
@@ -12,8 +12,14 @@ const Header = () => {
   const location = useLocation();
   const isSearchPage = location.pathname === "/search";
 
+  const navigate = useNavigate();
+
+  const redirectToLoginPage = () => {
+    navigate("/login");
+  }
+
   return (
-    <header className="h-28 lg:h-20 lg:shadow-md sticky top-0 flex justify-center gap-1 flex-col">
+    <header className="h-28 lg:h-20 lg:shadow-md bg-white sticky top-0 flex justify-center gap-1 flex-col">
       {!(isSearchPage && isMobile) && (
         <div className="px-4 flex justify-between items-center">
           <Link to={"/"}>
@@ -28,7 +34,7 @@ const Header = () => {
               <FaRegCircleUser size={30} />
             </button>
             <div className="hidden lg:flex items-center gap-10">
-              <Link to="/login" className="text-xl">Login</Link>
+              <button onClick={redirectToLoginPage}  className="text-xl">Login</button>
               <button className="flex items-center gap-2 bg-green-800 hover:bg-green-700 px-3 py-3 rounded-md text-white">
                 <div className="animate-bounce">
                   <BsCart4 size={26} />
