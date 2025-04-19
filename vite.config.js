ca66f3fd773aead16301b9517250af83501f9c36
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-19 16:53:35 +0530

    Some changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  ...(mode === "development" && {
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://binkeyit-server.vercel.app",
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  })
+}))
