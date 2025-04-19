2db60615f1d92120dc0ad49821eb66c74eafd8e3
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-19 15:11:18 +0530

    Make some changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://binkeyit-server.vercel.app",
         changeOrigin: true,
         secure: false
       }
