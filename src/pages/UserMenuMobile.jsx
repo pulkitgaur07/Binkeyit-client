fdb35f1265f2019fe8ec3d375b9f69113de1152a
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-19 12:29:54 +0530

    Mobile view Profile Correction.

--- src/pages/UserMenuMobile.jsx
+++ src/pages/UserMenuMobile.jsx
@@ -5,7 +5,7 @@ import { IoClose } from "react-icons/io5";
 const UserMenuMobile = () => {
   return (
     <section className='bg-white h-full w-full py-2'>
-        <button onClick={window.history.back()} className='text-neutral-800 block w-fit ml-auto '><IoClose size={20} /></button>
+        <button onClick={()=>window.history.back()} className='text-neutral-800 block w-fit ml-auto '><IoClose size={20} /></button>
         <div className='container mx-auto px-3 pb-8'>
             <UserMenu />
         </div>
