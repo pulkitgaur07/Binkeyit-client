2ae7ed321a4c4636104e0eac88100b4d10f1d412
Pulkit Gaur <pulkitgaur9450@gmail.com>
2025-04-20 11:10:38 +0530

    User related issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 import fetchUserDetails from "./utils/fetchUserDetails";
 import { setUserDetails } from "./store/userSlice";
 import { setAllCategory, setAllSubCategory, setLoadingCategory } from "./store/productSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SummaryApi from "./common/SummaryApi";
 import GlobalProvider from "./provider/GlobalProvider";
 import CartMobileLink from "./components/CartMobileLink";
@@ -17,6 +17,7 @@ function App() {
 
   const dispatch = useDispatch();
   const location = useLocation();
+  const user = useSelector((state) => state?.user);
 
   const fetchUser = async()=>{
     const userData = await fetchUserDetails();
@@ -56,7 +57,9 @@ function App() {
   };
 
   useEffect(()=>{
-    fetchUser();
+    if (user && user._id){
+      fetchUser();
+    }
     fetchCategory();
     fetchSubCategory();
   },[]);
